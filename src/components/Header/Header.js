3c3922a934d6dc810247c1832b4363d86364e340
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2023-11-23 01:21:57 -0500

    added alot of styling, taking break

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,60 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Header.scss";
+import logo from "../../assets/Logo/consolidation-icon-for-graphic-design-logo-free-vector.png";
 
 const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div className="header">
-      <div>
-        <Link to="/" className="header-logo">
-          <img src="" alt="Critic Consolidator Logo" />
-          <h2>Critic Consolidator</h2>
+      <div className="header__logo-container">
+        <Link to="/" className="header__logo">
+          <img
+            className="header__logo-image"
+            src={logo}
+            alt="Critic Consolidator Logo"
+          />
+          <h2 className="header__title">Critic Consolidator</h2>
         </Link>
       </div>
-      <div className="header-links">
-        <Link to="/movies" className="header-link">
+      <div className="header__nav">
+        <Link to="/movies" className="header__nav-link">
           Movies
         </Link>
-        <Link to="/games" className="header-link">
+        <Link to="/games" className="header__nav-link">
           Games
         </Link>
-        <Link to="/books" className="header-link">
+        <Link to="/books" className="header__nav-link">
           Books
         </Link>
-        <Link to="/local" className="header-link">
+        <Link to="/local" className="header__nav-link">
           Local
         </Link>
       </div>
+      <div className="header__mobile-menu" onClick={toggleMobileMenu}>
+        Categories
+      </div>
+      {isMobileMenuOpen && (
+        <div className="header__mobile-dropdown">
+          <Link to="/movies" className="header__nav-dropdown">
+            Movies
+          </Link>
+          <Link to="/games" className="header__nav-dropdown">
+            Games
+          </Link>
+          <Link to="/books" className="header__nav-dropdown">
+            Books
+          </Link>
+          <Link to="/local" className="header__nav-dropdown">
+            Local
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Homepagehero/Homepagehero.js
+++ src/components/Homepagehero/Homepagehero.js
@@ -1,23 +1,34 @@
 import "./Homepagehero.scss";
 import { Link } from "react-router-dom";
+import map from "../../assets/images/Google-Map-SEO.jpg";
 
 const HomePageHero = () => {
   return (
     <section className="homepage__hero-container">
+      <h2 className="homepage__hero-header">Review Categories</h2>
       <div className="homepage__hero">
-        <div className="homepage__hero-card homepage__hero-card--movies">
-          <h2>Movies</h2>
+        <div className="homepage__hero__card-container">
+          <h2 className="homepage__hero__card-header">Movies</h2>
+          <Link to="/movies">
+            <div className="homepage__hero__card homepage__hero__card--movies"></div>
+          </Link>
         </div>
-        <div className="homepage__hero-card homepage__hero-card--games">
-          <h2>Games</h2>
+        <div className="homepage__hero__card-container">
+          <h2 className="homepage__hero__card-header">Games</h2>
+          <Link to="/games">
+            <div className="homepage__hero__card homepage__hero__card--games"></div>
+          </Link>
         </div>
-        <div className="homepage__hero-card homepage__hero-card--books">
-          <h2>Books</h2>
+        <div className="homepage__hero__card-container">
+          <h2 className="homepage__hero__card-header">Books</h2>
+          <Link to="/books">
+            <div className="homepage__hero__card homepage__hero__card--books"></div>
+          </Link>
         </div>
       </div>
       <div class="homepage__local">
         <h2 class="homepage__local-header">Local Search</h2>
-        <img class="homepage__local-image" src="" alt="Hero Image"></img>
+        <img class="homepage__local-image" src={map} alt="Map Image"></img>
         <p class="homepage__local-text">Feature coming soon!</p>
       </div>
     </section>
